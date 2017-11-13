21d9ffe622743ca4ac7737459f5447da09bc29ae
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-11-13 09:35:45 -0700

    Made a random color generatror.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -8,36 +8,36 @@ import javax.swing.SpringLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
 	private JButton firstButton;
 	private SpringLayout baseLayout;
-	
+
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
 		firstButton = new JButton("YEET");
 		baseLayout = new SpringLayout();
-		
+
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
+
 	/**
-	 * Used to add all components to the subclass of JPanel
-	 * This installs them into the panel so they are seen in the GUI.
+	 * Used to add all components to the subclass of JPanel This installs them into the panel so they
+	 * are seen in the GUI.
 	 */
-	
+
 	private void setupPanel()
 	{
 		this.setBackground(Color.CYAN);
 		this.setLayout(baseLayout);
 		this.add(firstButton);
 	}
-	
+
 	/**
 	 * This is used to hold all the layout and arrangement code for SpringLayout.
 	 */
@@ -47,7 +47,7 @@ public class GUIPanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.WEST, firstButton, -85, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -10, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -10, SpringLayout.EAST, this);
-		
+
 	}
 
 	/**
@@ -55,6 +55,21 @@ public class GUIPanel extends JPanel
 	 */
 	private void setupListeners()
 	{
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+					changeBackgroundColor();
+			}
+		});
+	}
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
 		
+		this.setBackground(new Color(red, green, blue));
 	}
 }
